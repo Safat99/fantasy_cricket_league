8403155e0ee2ae5842463f1f48a4906c68240b6d
Safat99 <sarkersafat99@gmail.com>
2024-02-25 02:42:22 +0600

    first commit

---
+++ src/main/java/com/sarker/fcl_main/FclMainApplication.java
@@ -0,0 +1,13 @@
+package com.sarker.fcl_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FclMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FclMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarker/fcl_main/FclMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarker.fcl_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FclMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
